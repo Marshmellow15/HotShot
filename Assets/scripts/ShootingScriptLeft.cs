523cd699904a16f2b8b021a8d6dd0459b1373546
Demello <demellobrandon@gmail>
2018-08-22 13:25:16 -0500

    The First push ever

---
+++ Assets/scripts/ShootingScriptLeft.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingScriptLeft : MonoBehaviour {
+    public bool isFiringLeft;
+
+    public StandardBullet bullet;
+    public float bulletSpeed;
+
+    public float shotTimerL;
+    private float shotCounterL;
+
+    public Transform firePoint;
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isFiringLeft)
+        {
+            shotCounterL -= Time.deltaTime;
+            if (shotCounterL <= 0)
+            {
+                shotCounterL = shotTimerL;
+                StandardBullet newBullet = Instantiate(bullet, firePoint.position, firePoint.rotation) as StandardBullet;
+                newBullet.speed = bulletSpeed;
+            }
+        }
+        else
+        {
+            shotCounterL = 0;
+
+        }
+    }
+}
---
+++ Assets/scripts/ShootingScriptRight.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingScriptRight : MonoBehaviour {
+
+    public bool isFiringRight;
+
+    public StandardBullet bullet;
+    public float bulletSpeed;
+
+    public float shotTimerR;
+    private float shotCounterR;
+
+    public Transform firePoint;
+
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(isFiringRight)
+        {
+            shotCounterR -= Time.deltaTime;
+            if(shotCounterR <= 0)
+            {
+                shotCounterR = shotTimerR;
+                StandardBullet newBullet = Instantiate(bullet, firePoint.position, firePoint.rotation) as StandardBullet;
+                newBullet.speed = bulletSpeed;
+            }
+        }
+        else
+        {
+            shotCounterR = 0;
+
+        }
+	}
+}
