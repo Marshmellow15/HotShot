0449db18ad76fd1653290e1502d6a9bc4ed5eb46
Demello <demellobrandon@gmail>
2018-09-04 20:55:13 -0500

    updated movement script

---
+++ Assets/scripts/PlayerScripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public Transform player;
+    public Vector3 playerOffset;
+    public float movementSpeed;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        moveCamera();	
+	}
+
+    void moveCamera()
+    {
+        transform.position = Vector3.Lerp(transform.position, player.position + playerOffset, movementSpeed * Time.deltaTime);
+    }
+
+
+
+}
--- Assets/scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/scripts/PlayerScripts/PlayerMovement.cs
@@ -8,39 +8,21 @@ public class PlayerMovement : MonoBehaviour {
 
     public float moveSpeed;
     private Rigidbody PlayerRigidbody;
-
-    private Vector3 moveInput;
-    private Vector3 moveVelocity;
     public ShootingScriptRight rightHand;
     public ShootingScriptLeft leftHand;
-    private Camera mainCamera;
+    
 
 	void Start ()
     {
         PlayerRigidbody = GetComponent<Rigidbody>();
-        mainCamera = FindObjectOfType<Camera>();
+  
 	}
 
 
     void Update()
     {
-        //Movement
-        moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
-        moveVelocity = moveInput * moveSpeed;
-        //Looking 
-        Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
-
-        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
-        float rayLength;
-        //point character to camera
-        if (groundPlane.Raycast(cameraRay, out rayLength))
-        {
-            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
-            Debug.DrawLine(cameraRay.origin, pointToLook, Color.blue);
-
-
-            transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
-        }
+        Movement();
+        Rotation();
         //Right Hand shooting
         if (Input.GetMouseButtonDown(1))
         {
@@ -63,9 +45,25 @@ public class PlayerMovement : MonoBehaviour {
         }
     }
 
-    void FixedUpdate()
+    void Movement ()
     {
-        PlayerRigidbody.velocity = moveVelocity;
+        float horiz= Input.GetAxis("Horizontal");
+        float verti   = Input.GetAxis("Vertical");
+
+        Vector3 _movement = new Vector3(horiz, 0, verti);
+        transform.Translate(_movement * moveSpeed * Time.deltaTime, Space.World);
+
+    }
+    void Rotation()
+    {
+        RaycastHit _hit;
+        Ray _ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(_ray, out _hit))
+        {
+            transform.LookAt(new Vector3(_hit.point.x, transform.position.y, _hit.point.z));
+        }
+
     }
 
 
