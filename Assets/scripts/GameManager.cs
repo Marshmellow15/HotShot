f222417ae74e25e43688571adb43ac52782a9e29
Demello <demellobrandon@gmail>
2018-09-11 14:06:55 -0500

    game over and reload level

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -1,20 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class GameManager : MonoBehaviour {
 
+    public PlayerHealth health;
+    public float restartDelay = 5f;
+
+    Animator anim;
+    float restartTimer;
 
 
-    private bool gameOver;
-    private bool restart;
+    void Awake()
+    {
+        // Set up the reference.
+        anim = GetComponent<Animator>();
+    }
 
     void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If the player has run out of health...
+        if (health.currentHealth <= 0)
+        {
+            // ... tell the animator the game is over.
+            anim.SetTrigger("GameOver");
+
+            // .. increment a timer to count up to restarting.
+            restartTimer += Time.deltaTime;
+
+            // .. if it reaches the restart delay...
+            if (restartTimer >= restartDelay)
+            {
+                // .. then reload the currently loaded level.
+
+                SceneManager.LoadScene("Level1", LoadSceneMode.Single);
+            }
+        }
+    }
 }
